dea9d5b045bdf14b9d0ad2eab9f0675cdc8831c3
Muhammad Usman Bin Farid <99407553+UsmanUet276@users.noreply.github.com>
2023-01-29 19:28:37 +0500

    data

---
+++ Lab02-Task-01/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_Task_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "")
+            {
+                MessageBox.Show("Fill All text box");
+            }
+            else
+            {
+               
+                var con = Configuration.getInstance().getConnection();
+                SqlCommand cmd = new SqlCommand("Insert into student(RegistrationNumber, Name,Department, Session, CGPA, Address) values (@RegistrationNumber, @Name,@Department, @Session, @CGPA, @Address)", con);
+                cmd.Parameters.AddWithValue("@RegistrationNumber", textBox1.Text);
+                cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@Department", textBox3.Text);
+                cmd.Parameters.AddWithValue("@Session", int.Parse(textBox4.Text));
+                cmd.Parameters.AddWithValue("@CGPA", int.Parse(textBox5.Text));
+                cmd.Parameters.AddWithValue("@Address", textBox6.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully saved");
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Select * from student", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox9.Text.Length == 0 || textBox9.Text == "Enter Registration Number")
+            {
+
+                MessageBox.Show("Enter Valid/Correct Registration Number");
+            }
+            else
+            {
+                var con = Configuration.getInstance().getConnection();
+                SqlCommand cmd = new SqlCommand("DELETE FROM student WHERE RegistrationNumber = @RegistrationNumber", con);
+                cmd.Parameters.AddWithValue("@RegistrationNumber", textBox9.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully Deleted");
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox8.Text.Length == 0 || textBox8.Text == "Enter Registration Number")
+            {
+
+                MessageBox.Show("Enter Valid/Correct Registration Number");
+            }
+            else
+            {
+                var con = Configuration.getInstance().getConnection();
+                SqlCommand cmd = new SqlCommand("Select * from student where RegistrationNumber = @RegistrationNumber", con);
+                cmd.Parameters.AddWithValue("@RegistrationNumber", textBox8.Text);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt; ;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ Lab02-Task-01/Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab02_Task_01
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length == 0 || textBox1.Text == "Enter Registration Number" || textBox2.Text.Length == 0 || textBox2.Text == "Enter Data" || comboBox1.Text == "(Select)") {
+                MessageBox.Show("Enter/Select valid Info");
+            }
+            else
+            {
+                        if (comboBox1.Text == "Name")
+                        {
+                            var con = Configuration.getInstance().getConnection();
+                            SqlCommand cmd = new SqlCommand("UPDATE student SET Name = @Name WHERE RegistrationNumber = @RegistrationNumber", con);
+                            cmd.Parameters.AddWithValue("@RegistrationNumber", textBox1.Text);
+                            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Successfully Updated");
+                        }
+                        else if (comboBox1.Text == "Department")
+                        {
+                            var con = Configuration.getInstance().getConnection();
+                            SqlCommand cmd = new SqlCommand("UPDATE student SET Department = @Department WHERE RegistrationNumber = @RegistrationNumber", con);
+                            cmd.Parameters.AddWithValue("@RegistrationNumber", textBox1.Text);
+                            cmd.Parameters.AddWithValue("@Department", textBox2.Text);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Successfully Updated");
+                        }
+                        else if (comboBox1.Text == "Session")
+                        {
+                            var con = Configuration.getInstance().getConnection();
+                            SqlCommand cmd = new SqlCommand("UPDATE student SET Session = @Session WHERE RegistrationNumber = @RegistrationNumber", con);
+                            cmd.Parameters.AddWithValue("@RegistrationNumber", textBox1.Text);
+                            cmd.Parameters.AddWithValue("@Session", int.Parse(textBox2.Text));
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Successfully Updated");
+                        }
+                        else if (comboBox1.Text == "CGPA")
+                        {
+                            var con = Configuration.getInstance().getConnection();
+                            SqlCommand cmd = new SqlCommand("UPDATE student SET CGPA = @CGPA WHERE RegistrationNumber = @RegistrationNumber", con);
+                            cmd.Parameters.AddWithValue("@RegistrationNumber", textBox1.Text);
+                            cmd.Parameters.AddWithValue("@CGPA", int.Parse(textBox2.Text));
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Successfully Updated");
+                        }
+                        else if (comboBox1.Text == "Address")
+                        {
+                            var con = Configuration.getInstance().getConnection();
+                            SqlCommand cmd = new SqlCommand("UPDATE student SET Address = @Address WHERE RegistrationNumber = @RegistrationNumber", con);
+                            cmd.Parameters.AddWithValue("@RegistrationNumber", textBox1.Text);
+                            cmd.Parameters.AddWithValue("@Address", textBox2.Text);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Successfully Updated");
+                        }
+                
+            }
+        }
+    }
+}
